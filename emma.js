4af22b5ea751e7a7270add6f539c3dd09181c100
pemma6991 <113259923+pemma6991@users.noreply.github.com>
2022-09-10 08:03:39 -1100

    add me to

---
+++ emma.js
@@ -0,0 +1,18 @@
+
+
+ document.querySelector("button").addEventListener("click", function () {
+    var name="emmanuel";
+    var emg="emeka";
+    var f="favour";
+var emma=prompt("you must sign up to E-GLE your last name only");
+if (emma===name) {
+     alert ("sorry for distubing you continu your search");
+} else if(emma===emg) {
+    alert ("what are you doing in my web site abie you no like your life");
+} else if(emma===f) {
+    alert ("hope you are impress with my work")
+}
+else {
+    alert ("you have loged in to e-gle");
+}
+ });
